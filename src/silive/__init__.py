fef0df6137e83a67badb4fccce1ffbd3ddcc082a
nikitoys <65620955+nikitoys@users.noreply.github.com>
2026-05-28 15:06:30 +0300

    Export plotting helpers

--- src/silive/__init__.py
+++ src/silive/__init__.py
@@ -1,6 +1,7 @@
 """Silive proto-life simulation package."""
 
 from .model import ALL_GENES, PAIR_STABILITY, ProtoLife, SimulationConfig, compare_gene_sets, simulate
+from .plot import PhaseGrid, make_phase_grid, plot_phase_map, read_phase_map
 from .sweep import SweepConfig, linspace, run_sweep, write_csv
 
 __all__ = [
@@ -14,4 +15,8 @@ __all__ = [
     "linspace",
     "run_sweep",
     "write_csv",
+    "PhaseGrid",
+    "read_phase_map",
+    "make_phase_grid",
+    "plot_phase_map",
 ]
