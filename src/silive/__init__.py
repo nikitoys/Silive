0105c1467515ef56ee286c652aa33119d0d23d91
nikitoys <65620955+nikitoys@users.noreply.github.com>
2026-05-28 14:59:45 +0300

    Export sweep helpers

--- src/silive/__init__.py
+++ src/silive/__init__.py
@@ -1,6 +1,7 @@
 """Silive proto-life simulation package."""
 
-from .model import ALL_GENES, PAIR_STABILITY, ProtoLife, SimulationConfig, simulate, compare_gene_sets
+from .model import ALL_GENES, PAIR_STABILITY, ProtoLife, SimulationConfig, compare_gene_sets, simulate
+from .sweep import SweepConfig, linspace, run_sweep, write_csv
 
 __all__ = [
     "ALL_GENES",
@@ -9,4 +10,8 @@ __all__ = [
     "SimulationConfig",
     "simulate",
     "compare_gene_sets",
+    "SweepConfig",
+    "linspace",
+    "run_sweep",
+    "write_csv",
 ]
